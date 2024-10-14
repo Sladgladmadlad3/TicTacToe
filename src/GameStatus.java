c0b605c1b3a62713d3eee8dc2a7662e4c1b75500
Michael Franklin <mf021036@gmail.com>
2024-10-14 15:44:30 -0600

    feat: add checkBoard method for access

--- src/GameStatus.java
+++ src/GameStatus.java
@@ -1,3 +1,14 @@
 public class GameStatus {
+    public void checkBoard(gameBoard board) {
+        // Access the private 2D array via the getter method
+        char[][] boardState = board.getGameBoard();
 
+        for (char[] row: boardState) {
+            for(char cell: row) {
+                System.out.print(cell);
+            }
+            System.out.println();
+        }
+
+    }
 }
