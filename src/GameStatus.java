eb12d4e653502bb473da612c0079ec639190a9d3
Michael Franklin <mf021036@gmail.com>
2024-10-17 10:33:10 -0600

    feat: add checkWin (Master class), checkHorizontal. checkHorizontal logic implemented and simplicity

--- src/GameStatus.java
+++ src/GameStatus.java
@@ -1,55 +1,40 @@
 public class GameStatus {
-
-    public static int pieceCount = 0;
+    private gameBoard board = new gameBoard(); //instance of the gameBoard class
 
     /**
-     * 
-     * @param board
-     * @param XorO
-     * @return
+     * Checks if the game is over (win or draw). Master method.
+     * @return True if the game is over, false otherwise.
      */
-    /*
-    public static boolean checkWin(gameBoard board, char XorO) {
-        char[][] boardState = board.getGameBoard();
-        //horizontal checks
-         for (int i = 0; i < boardState.length; i+=2) {
-             int count = 0;
-             for (int j = 0; j < boardState[i].length; j+=2) {
-                 if (boardState[i][j] == XorO) {
-                     count++;
-                 }
-             }
-             if(count == 3) {
-                 return true;
-             }
-        }
-        //Vertical checks
-        for (int i = 0; i < boardState[0].length; i+=2) {
-            int count = 0;
-            for (int j = 0; j < boardState.length; j+=2) {
-                if (boardState[j][i] == XorO) {
-                    count++;
-                }
-            }
-            if(count == 3) {
+    public boolean checkWin() {
+        return checkHorizontal(board);
+    }
+
+    /**
+     * Checks if there is a win on the horizontal axis.
+     *
+     * @param board - The game board to check for a win.
+     * @return - True if there is a win on the horizontal axis, false otherwise.
+     */
+    private boolean checkHorizontal(gameBoard board) {
+        for(char[] row: board.getBoard()) {
+            if(row[0] == row[1] && row[1] == row[2]) {
                 return true;
             }
-        }
-        //Diagonal Checks
-        if(boardState[0][0] == XorO && boardState[2][2] == XorO && boardState[4][4] == XorO) {
-            return true;
-        }
-
-        if(boardState[4][0] == XorO && boardState[2][2] == XorO && boardState[0][4] == XorO) {
-
-            return true;
         }
         return false;
     }
 
-    public static boolean checkDraw() {
-        return pieceCount >= 8;
+
+    private boolean checkVertical() {
+        return false;
     }
-    */
+
+    private boolean checkDiagonal() {
+        return false;
+    }
+
+    public boolean checkDraw() {
+        return false;
+    }
 
 }
