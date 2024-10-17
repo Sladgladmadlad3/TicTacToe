d580a36e0a6d504452d7105f15899fb6a661b44b
Michael Franklin <mf021036@gmail.com>
2024-10-17 12:54:27 -0600

    perf: made a variable tictactoeBoard that makes to code look more clean

--- src/GameStatus.java
+++ src/GameStatus.java
@@ -1,6 +1,7 @@
 public class GameStatus {
     public static int piecesPlaced = 0;
-    private gameBoard board = new gameBoard(); //instance of the gameBoard class
+    private final gameBoard board = new gameBoard(); //instance of the gameBoard class
+    private final char[][] tictactoeBoard = board.getBoard();
 
     /**
      * Checks if the game is over (win or draw). Master method.
@@ -16,7 +17,7 @@ public class GameStatus {
      * @return - True if there is a win on the horizontal axis, false otherwise.
      */
     private boolean checkHorizontal() {
-        for(char[] row: board.getBoard()) {
+        for(char[] row: tictactoeBoard) {
             if(row[0] == row[1] && row[1] == row[2]) {
                 return true;
             }
@@ -29,8 +30,8 @@ public class GameStatus {
      * @return - True if there is a win on the vertical axis, false otherwise.
      */
     private boolean checkVertical() {
-        for(int i=0; i < (board.getBoard()[0].length); i++) {
-            if(board.getBoard()[0][i] == board.getBoard()[1][i] && board.getBoard()[1][i] == board.getBoard()[2][i]) {
+        for(int i=0; i < (tictactoeBoard[0].length); i++) {
+            if(tictactoeBoard[0][i] == tictactoeBoard[1][i] && tictactoeBoard[1][i] == tictactoeBoard[2][i]) {
                 return true;
             }
         }
@@ -43,8 +44,8 @@ public class GameStatus {
      */
     private boolean checkDiagonal() {
 
-        return board.getBoard()[0][0] == board.getBoard()[1][1] && board.getBoard()[1][1] == board.getBoard()[2][2] ||
-                board.getBoard()[0][2] == board.getBoard()[1][1] && board.getBoard()[1][1] == board.getBoard()[2][0];
+        return tictactoeBoard[0][0] == tictactoeBoard[1][1] && tictactoeBoard[1][1] == tictactoeBoard[2][2] ||
+                tictactoeBoard[0][2] == tictactoeBoard[1][1] && tictactoeBoard[1][1] == tictactoeBoard[2][0];
     }
 
     /**
