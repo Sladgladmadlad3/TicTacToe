66946c101cab778908f0900f7fab44506e46c839
Michael Franklin <mf021036@gmail.com>
2024-10-17 18:31:39 -0600

    refactor: deleted comments to improvate readability. That code is no longer needed

--- src/Main.java
+++ src/Main.java
@@ -17,50 +17,7 @@ public class Main {
         board.printGameBoard(board.getBoard());
         System.out.println(gameStatus.checkWin());
         System.out.println(gameStatus.checkDraw());
-        /*
-        Scanner scanner = new Scanner(System.in);
-        gameBoard board = new gameBoard();
-        boolean gameOver = false;
 
-        while(!gameOver) {
-            // Print the gameboard
-            board.printGameBoard();
-
-            //Ask the player for input
-            System.out.print("Player X, choose your position (1-9): ");
-            int position = scanner.nextInt();
-
-            // Set the piece on the board
-            board.setGamePiece(position, 'X');
-
-            if(GameStatus.checkWin(board, 'X')) {
-                System.out.println("Player X wins!");
-                break;
-            } else if (GameStatus.checkDraw()) {
-                System.out.println("It's a draw!");
-                break;
-            }
-
-            board.printGameBoard();
-
-            System.out.print("Player O, choose your position (1-9): ");
-            position = scanner.nextInt();
-
-            // Set the piece on the board
-            board.setGamePiece(position, 'O');
-
-            if(GameStatus.checkWin(board, 'O')) {
-                System.out.println("Player O wins!");
-                gameOver = true;
-            } else if (GameStatus.checkDraw()) {
-                System.out.println("It's a draw!");
-                gameOver = true;
-            }
-
-        }
-
-        scanner.close();
-        */
     }
     
 }
