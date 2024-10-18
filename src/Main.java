a192e6ed4839d96262d2f190e29298c46a601c36
Michael Franklin <mf021036@gmail.com>
2024-10-18 07:18:48 -0600

    feat: User input logic is implemented. Main class is now ready for AI logic

--- src/Main.java
+++ src/Main.java
@@ -8,15 +8,33 @@ public class Main {
      * @param args Command line arguments (not used in this game)
      */
     public static void main(String[] args) {
-        gameBoard board = new gameBoard(); // instance of the gameBoard class
-        GameStatus gameStatus = new GameStatus(); // instance of the GameStatus class
-        board.initializeBoard();
-        board.setGamePiece(1, 1);
-        board.setGamePiece(2, 2);
-        board.setGamePiece(3, 3);
-        board.printGameBoard(board.getBoard());
-        System.out.println(gameStatus.checkWin());
-        System.out.println(gameStatus.checkDraw());
+        gameBoard tictacToe = new gameBoard(); // Instance of gameBoard
+        char[][] board = tictacToe.getBoard(); // getter method of gameBoard
+        boolean gameOver = false;
+        System.out.println("Welcome to TicTacToe!");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Would you like to be X or O:");
+        char piece = scanner.next().charAt(0);
+
+        while (!gameOver) {
+            tictacToe.initializeBoard();
+
+            Scanner cord = new Scanner(System.in);
+
+            System.out.println("Enter the X-coordinate (1-3): ");
+            int x = cord.nextInt();
+
+            System.out.println("Enter the Y-coordinate (1-3): ");
+            int y = cord.nextInt();
+
+            tictacToe.setGamePiece(x, y);
+            tictacToe.printGameBoard(board);
+
+
+        }
+
+
 
     }
     
